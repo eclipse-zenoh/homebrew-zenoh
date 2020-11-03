0ba1dba0052299258e77faf390d68709744a6f49
Olivier Hécart <olivier.hecart@adlinktech.com>
2020-11-03 21:51:37 +0100

    Version 0.5.0-beta.5

--- libzenohc.rb
+++ libzenohc.rb
@@ -1,9 +1,9 @@
 class Libzenohc < Formula
   desc "zenoh-c API (geo-distributed pub/sub/query/storage of data)"
   homepage "https://zenoh.io"
-  url "https://download.eclipse.org/zenoh/zenoh/0.5.0-beta.3/zenoh-c/eclipse-zenoh-c-0.5.0-beta.3-macosx10.7-x86-64.tar.gz"
-  version "0.5.0-beta.3"
-  sha256 "3234260ae70ac1a4fd5251eebda61fcc36aea8a66e885b69e076959c3b163ba1"
+  url "https://download.eclipse.org/zenoh/zenoh-c/0.5.0-b5/eclipse-zenoh-c-0.5.0-b5-macosx10.7-x86-64.tar.gz"
+  version "0.5.0-beta.5"
+  sha256 "369a9edf3098bac97255a7a72607a184202ac244e8caef887015dc73672f175e"
 
   def install
     lib.install "libzenohc.dylib"
--- zenoh-storages.rb
+++ zenoh-storages.rb
@@ -1,9 +1,9 @@
 class ZenohStorages < Formula
   desc "Eclipse zenoh storages plugin (geo-distributed pub/sub/query/storage of data)"
   homepage "https://zenoh.io"
-  url "https://download.eclipse.org/zenoh/zenoh/0.5.0-beta.3/eclipse-zenoh-0.5.0-beta.3-macosx10.7-x86-64.tgz"
-  version "0.5.0-beta.3"
-  sha256 "cfe140bba08506fd92939b6f0288a656eb25843a2e70b097e4e8e2304754987e"
+  url "https://download.eclipse.org/zenoh/zenoh/0.5.0-beta.5/eclipse-zenoh-0.5.0-beta.5-macosx10.7-x86-64.tgz"
+  version "0.5.0-beta.5"
+  sha256 "eafd06b1953e3c79b7ef13591b622484507ab68a78bcee4caa7886df79183fb3"
 
   depends_on "zenohd"
 
