78d487daa24e57891ee7d3bbf0fdcd885cd0e636
Julien Enoch <julien.enoch@adlinktech.com>
2022-10-06 16:09:03 +0200

    Fix url for zenoh-c aarch64

--- libzenohc.rb
+++ libzenohc.rb
@@ -9,7 +9,7 @@ class Libzenohc < Formula
       sha256 "f7551c4827d726551989244bf626d2abe982e1c16306f7c432ca7c150181e45f"
     end
     on_arm do
-      url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/aarch64-apple-darwin/zenoh-c-0.6.0-beta.1-aarch64-apple-darwin.zip"
+      url "https://download.eclipse.org/zenoh/zenoh-c/0.6.0-beta.1/aarch64-apple-darwin/zenoh-c-0.6.0-beta.1-aarch64-apple-darwin.zip"
       sha256 "a87653ec7a6b68392c19be382cf3a3e22f35968e5e3b1392749b18cfa490d376"
     end
   end
