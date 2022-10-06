85e1cb859d2c20296827203de9bd166c6c783d06
Julien Enoch <julien.enoch@adlinktech.com>
2022-10-06 15:20:26 +0200

    Version 0.6.0-beta.1 (x86_64 & aarch64)

--- libzenohc.rb
+++ libzenohc.rb
@@ -1,9 +1,18 @@
 class Libzenohc < Formula
   desc "zenoh-c API (geo-distributed pub/sub/query/storage of data)"
   homepage "https://zenoh.io"
-  url "https://download.eclipse.org/zenoh/zenoh-c/0.6.0-beta.1/x86_64-apple-darwin/zenoh-c-0.6.0-beta.1-x86_64-apple-darwin.zip"
   version "0.6.0-beta.1"
-  sha256 "f7551c4827d726551989244bf626d2abe982e1c16306f7c432ca7c150181e45f"
+
+  on_macos do
+    on_intel do
+      url "https://download.eclipse.org/zenoh/zenoh-c/0.6.0-beta.1/x86_64-apple-darwin/zenoh-c-0.6.0-beta.1-x86_64-apple-darwin.zip"
+      sha256 "f7551c4827d726551989244bf626d2abe982e1c16306f7c432ca7c150181e45f"
+    end
+    on_arm do
+      url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/aarch64-apple-darwin/zenoh-c-0.6.0-beta.1-aarch64-apple-darwin.zip"
+      sha256 "a87653ec7a6b68392c19be382cf3a3e22f35968e5e3b1392749b18cfa490d376"
+    end
+  end
 
   def install
     lib.install "libzenohc.dylib"
--- zenoh.rb
+++ zenoh.rb
@@ -1,9 +1,18 @@
 class Zenoh < Formula
   desc "Eclipse Zenoh (geo-distributed pub/sub/query/storage of data)"
   homepage "https://zenoh.io"
-  url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/x86_64-apple-darwin/zenoh-0.6.0-beta.1-x86_64-apple-darwin.zip"
   version "0.6.0-beta.1"
-  sha256 "b09a881b9ffdc76db64bc67d1bf064858e696ea09c5f3b5a2c932edc1bb65220"
+
+  on_macos do
+    on_intel do
+      url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/x86_64-apple-darwin/zenoh-0.6.0-beta.1-x86_64-apple-darwin.zip"
+      sha256 "b09a881b9ffdc76db64bc67d1bf064858e696ea09c5f3b5a2c932edc1bb65220"
+    end
+    on_arm do
+      url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/aarch64-apple-darwin/zenoh-0.6.0-beta.1-aarch64-apple-darwin.zip"
+      sha256 "95ab32b00544519c1eb7de0c7e5ac638e29e6adb04d6c1356b47e265b28bedc7"
+    end
+  end
 
   depends_on "zenohd"
   depends_on "zenoh-plugin-rest" => :recommended
