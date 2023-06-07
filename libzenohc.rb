64e472e729b309c981f39a9723bac1348b9717cd
OlivierHecart <olivier.hecart@adlinktech.com>
2023-06-07 16:43:05 +0200

    Version 0.7.2-rc

--- libzenohc.rb
+++ libzenohc.rb
@@ -1,16 +1,16 @@
 class Libzenohc < Formula
   desc "zenoh-c API (geo-distributed pub/sub/query/storage of data)"
   homepage "https://zenoh.io"
-  version "0.6.0-beta.1"
+  version "0.7.2-rc"
 
   on_macos do
     on_intel do
-      url "https://download.eclipse.org/zenoh/zenoh-c/0.6.0-beta.1/x86_64-apple-darwin/zenoh-c-0.6.0-beta.1-x86_64-apple-darwin.zip"
-      sha256 "f7551c4827d726551989244bf626d2abe982e1c16306f7c432ca7c150181e45f"
+      url "https://download.eclipse.org/zenoh/zenoh-c/0.7.2-rc/x86_64-apple-darwin/zenoh-c-0.7.2-rc-x86_64-apple-darwin.zip"
+      sha256 "9568dbc4d34968d3345d8688742ca0b39d12a8dd81b1122901a7fc876a1fa0db"
     end
     on_arm do
-      url "https://download.eclipse.org/zenoh/zenoh-c/0.6.0-beta.1/aarch64-apple-darwin/zenoh-c-0.6.0-beta.1-aarch64-apple-darwin.zip"
-      sha256 "a87653ec7a6b68392c19be382cf3a3e22f35968e5e3b1392749b18cfa490d376"
+      url "https://download.eclipse.org/zenoh/zenoh-c/0.7.2-rc/aarch64-apple-darwin/zenoh-c-0.7.2-rc-aarch64-apple-darwin.zip"
+      sha256 "4d611c0161b28dae12e3c5fabc2cf4823b4c3544acef9fe53e6a4911aff2fb1b"
     end
   end
 
--- zenoh-plugin-storages.rb
+++ zenoh-plugin-storages.rb
@@ -1,16 +1,16 @@
 class ZenohPluginStorages < Formula
   desc "Eclipse zenoh storages plugin (geo-distributed pub/sub/query/storage of data)"
   homepage "https://zenoh.io"
-  version "0.6.0-beta.1"
+  version "0.7.2-rc"
 
   on_macos do
     on_intel do
-      url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/x86_64-apple-darwin/zenoh-0.6.0-beta.1-x86_64-apple-darwin.zip"
-      sha256 "b09a881b9ffdc76db64bc67d1bf064858e696ea09c5f3b5a2c932edc1bb65220"
+      url "https://download.eclipse.org/zenoh/zenoh/0.7.2-rc/x86_64-apple-darwin/zenoh-0.7.2-rc-x86_64-apple-darwin.zip"
+      sha256 "0146f7d41459f7e16696ec17005e4e9ded4496f747707b9f8520e9a59b92ef59"
     end
     on_arm do
-      url "https://download.eclipse.org/zenoh/zenoh/0.6.0-beta.1/aarch64-apple-darwin/zenoh-0.6.0-beta.1-aarch64-apple-darwin.zip"
-      sha256 "95ab32b00544519c1eb7de0c7e5ac638e29e6adb04d6c1356b47e265b28bedc7"
+      url "https://download.eclipse.org/zenoh/zenoh/0.7.2-rc/aarch64-apple-darwin/zenoh-0.7.2-rc-aarch64-apple-darwin.zip"
+      sha256 "f6d169d57ff3e585cf4988dfb0479464f4fdb1241370b39b441d6bb71f660538"
     end
   end
 
