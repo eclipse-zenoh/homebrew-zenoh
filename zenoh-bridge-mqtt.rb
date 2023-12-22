1a846dbd1c1034b759f7c66b91a27a73c62f79ed
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2023-12-22 12:40:20 +0100

    Add Apple Silicon build for zenoh-plugin-mqtt and zenoh-backend-mqtt

--- zenoh-bridge-mqtt.rb
+++ zenoh-bridge-mqtt.rb
@@ -6,7 +6,11 @@ class ZenohBridgeMqtt < Formula
   on_macos do
     on_intel do
       url "https://download.eclipse.org/zenoh/zenoh-plugin-mqtt/0.10.1-rc/x86_64-apple-darwin/zenoh-bridge-mqtt-0.10.1-rc-x86_64-apple-darwin.zip"
-      sha256 "3985c5b65e5741f056e0b9ad31ec25003abfb4f8e32227c4ce30c735cbdf5c61"
+      sha256 "1cbf24ff816c435f7465c6bedcc10020e7d3727bed541d8ef6a145f0160efa34"
+    end
+    on_arm do
+      url "https://download.eclipse.org/zenoh/zenoh-plugin-mqtt/0.10.1-rc/aarch64-apple-darwin/zenoh-bridge-mqtt-0.10.1-rc-aarch64-apple-darwin.zip"
+      sha256 "c13701992502615ea4372e57b17194d3dd76a65aab2a9f7729cbb1f48818e40c"
     end
   end
 
--- zenoh-plugin-mqtt.rb
+++ zenoh-plugin-mqtt.rb
@@ -6,7 +6,11 @@ class ZenohPluginMqtt < Formula
   on_macos do
     on_intel do
       url "https://download.eclipse.org/zenoh/zenoh-plugin-mqtt/0.10.1-rc/x86_64-apple-darwin/zenoh-plugin-mqtt-0.10.1-rc-x86_64-apple-darwin.zip"
-      sha256 "7aaf33f6f83301a96353b13e3c5afd274f2f2eaac41fb87d67f5d5539efa7823"
+      sha256 "3fe65c751767e4672da81c1bb0abd7210cf2af54fd1f4ffb5ff99001e40373aa"
+    end
+    on_arm do
+      url "https://download.eclipse.org/zenoh/zenoh-plugin-mqtt/0.10.1-rc/aarch64-apple-darwin/zenoh-plugin-mqtt-0.10.1-rc-aarch64-apple-darwin.zip"
+      sha256 "28a813446e12fc6f4526277f5aa8cd47d40940508fd269028b2c804521473482"
     end
   end
 
