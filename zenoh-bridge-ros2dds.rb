106c648a6467935c2ad6c491c1e435884cdf5a06
Julien Enoch <julien.e@zettascale.tech>
2024-01-23 19:24:39 +0100

    Release zenoh-plugin-ros2dds/zenoh-bridge-ros2dds 0.10.1-rc.2

--- zenoh-bridge-ros2dds.rb
+++ zenoh-bridge-ros2dds.rb
@@ -1,16 +1,16 @@
 class ZenohBridgeRos2dds < Formula
   desc "Eclipse zenoh ros2dds bridge"
   homepage "https://zenoh.io"
-  version "0.10.1-rc"
+  version "0.10.1-rc.2"
 
   on_macos do
     on_intel do
-      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc/x86_64-apple-darwin/zenoh-bridge-ros2dds-0.10.1-rc-x86_64-apple-darwin.zip"
-      sha256 "ddb5158786387cb8d8344c85893625865c3c2f13243750cf982cca87e479d451"
+      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc.2/x86_64-apple-darwin/zenoh-bridge-ros2dds-0.10.1-rc.2-x86_64-apple-darwin.zip"
+      sha256 "c58ea2d64175e1cde3b0231c651521f6e395ea80ed82533a03b7c09d9001e20a"
     end
     on_arm do
-      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc/aarch64-apple-darwin/zenoh-bridge-ros2dds-0.10.1-rc-aarch64-apple-darwin.zip"
-      sha256 "1dedc2e483e8478ca6ec2a349121f4b6103ef43ae6ccde66f03fc7af41e9cf63"
+      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc.2/aarch64-apple-darwin/zenoh-bridge-ros2dds-0.10.1-rc.2-aarch64-apple-darwin.zip"
+      sha256 "ba6e207310e32d2d7f97fcb51e00670898b60ed41b268ec9a2448f838b839402"
     end
   end
 
--- zenoh-plugin-ros2dds.rb
+++ zenoh-plugin-ros2dds.rb
@@ -1,16 +1,16 @@
 class ZenohPluginRos2dds < Formula
   desc "Eclipse zenoh ros2dds plugin"
   homepage "https://zenoh.io"
-  version "0.10.1-rc"
+  version "0.10.1-rc.2"
 
   on_macos do
     on_intel do
-      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc/x86_64-apple-darwin/zenoh-plugin-ros2dds-0.10.1-rc-x86_64-apple-darwin.zip"
-      sha256 "55f623376ef7e5a5239486d0598d2e25eec2835f5c68ca8698174e08ea82379f"
+      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc.2/x86_64-apple-darwin/zenoh-plugin-ros2dds-0.10.1-rc.2-x86_64-apple-darwin.zip"
+      sha256 "5140d22ace45d6f5a662db4347716896e85791ed89a8cd33e6bf9d4d02c83152"
     end
     on_arm do
-      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc/aarch64-apple-darwin/zenoh-plugin-ros2dds-0.10.1-rc-aarch64-apple-darwin.zip"
-      sha256 "ccee52e4784fa6a2fd604f032ad1942b6813e60cb6f4561781afaddc3379be21"
+      url "https://download.eclipse.org/zenoh/zenoh-plugin-ros2dds/0.10.1-rc.2/aarch64-apple-darwin/zenoh-plugin-ros2dds-0.10.1-rc.2-aarch64-apple-darwin.zip"
+      sha256 "ebae47e2c98ede0cb0b6889292d769de30e317b0627760cc86b3265db2b32cc6"
     end
   end
 
