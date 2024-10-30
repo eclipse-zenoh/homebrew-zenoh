38a801004345e7b4df1cf0fd05d334f3b898ef65
Diogo Mendes Matsubara <diogo.matsubara@zettascale.tech>
2024-10-30 16:49:47 +0100

    fix: change location of libzenohc.dylib
    new packages built by cmake/cpack add the dylib under lib/

--- libzenohc.rb
+++ libzenohc.rb
@@ -18,7 +18,7 @@ class Libzenohc < Formula
   end
 
   def install
-    lib.install "libzenohc.dylib"
+    lib.install "lib/libzenohc.dylib"
     include.install "include/zenoh.h"
     include.install "include/zenoh_commons.h"
     include.install "include/zenoh_concrete.h"
